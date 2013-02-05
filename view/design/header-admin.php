2f3789db2700acf263f107be90e21c3a135af16d
Pierre-Alexis de Solminihac <pa@solminihac.fr>
2013-02-05 01:48:48 +0100

    restoring skinbo-1.3 branch

--- view/design/header-admin.php
+++ view/design/header-admin.php
@@ -1,6 +1,6 @@
 <?php
 $request = $this->getRequest();
-if (!$request->AJAX) {
+if (!$request['AJAX']) {
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
@@ -12,7 +12,7 @@ if (!$request->AJAX) {
         <meta http-equiv="Content-Type" content="application/xhtml+xml;charset=<?php echo __HTML_ENCODING__; ?>" />
 <?php
     if (Clementine::$config['module_jstools']['use_google_cdn']) {
-        $this->getModel('cssjs')->register_js('jquery', array('src' => 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js'));
+        $this->getModel('cssjs')->register_js('jquery', array('src' => 'https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js'));
     } else {
         $this->getModel('cssjs')->register_js('jquery', array('src' => __WWW_ROOT_JSTOOLS__ . '/skin/jquery/jquery.min.js'));
     }
@@ -31,24 +31,22 @@ if (!$request->AJAX) {
         <div id="general">
             <div id="left">
                 <div class="logo">
-                    <a target="_blank" href="<?php echo __WWW__; ?>">
-                        <?php echo Clementine::$config['clementine_global']['site_name']; ?>
-                    </a>
+                    <?php echo Clementine::$config['clementine_global']['site_name']; ?>
 
                     <!-- liens de changement de langue -->
                     <div class="langue">
 <?php
-    $lang_dispo = array_keys($request->EQUIV);
+    $lang_dispo = array_keys($request['EQUIV']);
     if (count($lang_dispo) > 1) {
         foreach ($lang_dispo as $lang) {
 ?>
                         <a <?php
-            if ($request->LANG == $lang) {
+            if ($request['LANG'] == $lang) {
 ?>
 class="active"
 <?php 
             }
-            ?> href="<?php echo $request->EQUIV[$lang]; ?>"><img src="<?php echo __WWW_ROOT_SKINBO__ ?>/skin/images/<?php echo $lang; ?>.png" /></a>
+            ?> href="<?php echo $request['EQUIV'][$lang]; ?>"><img src="<?php echo __WWW_ROOT_SKINBO__ ?>/skin/images/<?php echo $lang; ?>.png" /></a>
 <?php 
         }
     }
@@ -61,7 +59,7 @@ class="active"
     // on n'affiche le lien de deconnexion que si l'utilisateur est bien loggue
     if ($this->getModel('users')->getAuth()) {
 ?>
-                    <a href="<?php echo $this->getModel('users')->getUrlLogout(); ?>">Déconnexion</a>
+                    |&nbsp;<a href="<?php echo $this->getModel('users')->getUrlLogout(); ?>">Déconnexion</a>
 <?php 
     }
 ?>
@@ -80,7 +78,7 @@ class="active"
 ?>
                 <ul id="main-nav">
                     <li>
-                        <a target="_blank" href="<?php echo __WWW__; ?>" class="<?php echo ($active == "accueil") ? 'current' : ''; ?>">Voir le site</a>
+                        <a target="_blank" href="<?php echo __WWW__; ?>" class="nav-top-item no-submenu <?php echo ($active == "accueil") ? 'current' : ''; ?>">Voir le site</a>
                     </li>
 <?php
     // on n'affiche le menu que si l'utilisateur est bien loggue
